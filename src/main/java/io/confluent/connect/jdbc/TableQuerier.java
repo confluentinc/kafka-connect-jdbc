0f1971351b7afd4ae69a93dc024f31c098d57ab3
Greg Fodor <gfodor@gmail.com>
2016-06-07 06:13:08 +0000

    Throw exception if isn't PostgreSQL-specific issue

--- src/main/java/io/confluent/connect/jdbc/TableQuerier.java
+++ src/main/java/io/confluent/connect/jdbc/TableQuerier.java
@@ -85,6 +85,8 @@ abstract class TableQuerier implements Comparable<TableQuerier> {
           log.info("Stale prepared statement, re-creating.");
           createPreparedStatement(db);
           resultSet = executeQuery();
+        } else {
+          throw e;
         }
       }
 

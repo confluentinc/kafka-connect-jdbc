bc915bf2d76da8d9f2b22af2689fef91c797b919
Rahul Varma <rahulv2504@gmail.com>
2017-06-29 16:41:31 -0700

    Fetching of pk fields temporarily removed to prevent conflicts

--- src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
+++ src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
@@ -135,26 +135,6 @@ public class JdbcSourceTask extends SourceTask {
 
       log.info("Mode for table: "+tableOrQuery+" is "+tableMode);
 
-      Connection connection = cachedConnectionProvider.getValidConnection();
-
-      final Set<String> pkColumns = new HashSet<>();
-      ResultSet pkResultSet=null;
-      try {
-        //final DatabaseMetaData dbMetaData = connection.getMetaData();
-        //final String product = dbMetaData.getDatabaseProductName();
-        final String catalog = connection.getCatalog();
-        final String schema = connection.getSchema();
-        pkResultSet = connection.getMetaData().getPrimaryKeys(catalog,schema,tableOrQuery);
-
-        //stmt.close();
-        while (pkResultSet.next()) {
-          final String colName = pkResultSet.getString("COLUMN_NAME");
-          pkColumns.add(colName);
-        }
-        log.info("All Pk Info for table: "+tableOrQuery+" - "+pkColumns);
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
       // Store all column names to be anonymized in anonymizeList
       anonymizeMap = null;
       try {

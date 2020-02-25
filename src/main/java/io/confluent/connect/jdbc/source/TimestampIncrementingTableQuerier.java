52609fcfb900d37cd7293ecec4fa6aedd540df98
Jinxin Liu <jinxin.liu@Jinxin-Lius-MBP15.local>
2020-02-25 12:38:30 -0800

    CC-7637: change from DEBUG level to TRACE level

--- src/main/java/io/confluent/connect/jdbc/source/TimestampIncrementingTableQuerier.java
+++ src/main/java/io/confluent/connect/jdbc/source/TimestampIncrementingTableQuerier.java
@@ -176,7 +176,7 @@ public class TimestampIncrementingTableQuerier extends TableQuerier implements C
   @Override
   protected ResultSet executeQuery() throws SQLException {
     criteria.setQueryParameters(stmt, this);
-    log.debug("Statement to execute: {}", stmt.toString());
+    log.trace("Statement to execute: {}", stmt.toString());
     return stmt.executeQuery();
   }
 

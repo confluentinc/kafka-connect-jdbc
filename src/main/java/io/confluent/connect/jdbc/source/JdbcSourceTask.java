2cd832525bbc849b63675c26f0b656ee1b81004b
rajat189 <rajat.sharma@peak.ai>
2020-10-09 15:48:06 +0530

    event status

--- src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
+++ src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
@@ -360,14 +360,14 @@ public class JdbcSourceTask extends SourceTask {
           // send event to SNS topic
           String topicArn = config.getString(JdbcSourceTaskConfig.SNS_TOPIC_ARN_CONFIG);
           if (!topicArn.equals("")) {
-            String topicName = config.getString(JdbcSourceTaskConfig.TOPIC_PREFIX_CONFIG);  
+            String topicName = config.getString(JdbcSourceTaskConfig.TOPIC_PREFIX_CONFIG);
             List<String> tableList = config.getList(
                 JdbcSourceTaskConfig.TABLES_CONFIG);
             if (tableList.size() > 0) {
               topicName += tableList.get(0).split("\\.")[1].replace("`", "");
             }
             Map<String, String> payload = new HashMap<String, String>();
-            payload.put("event", "success");
+            payload.put("status", "success");
             payload.put("topic", topicName);
             payload.put("feedId", config.getString(JdbcSourceTaskConfig.FEED_ID_CONFIG));
             payload.put("feedRunId", config.getString(JdbcSourceTaskConfig.FEED_RUN_ID_CONFIG));
@@ -420,7 +420,7 @@ public class JdbcSourceTask extends SourceTask {
             topicName += tableList.get(0).split("\\.")[1].replace("`", "");
           }
           Map<String, String> payload = new HashMap<String, String>();
-          payload.put("event", "failure");
+          payload.put("status", "failure");
           payload.put("error", sqle.getMessage());
           payload.put("topic", topicName);
           payload.put("feedId", config.getString(JdbcSourceTaskConfig.FEED_ID_CONFIG));

21f849f9683a8f51228c9bdb3f68c45105c73557
rajat189 <rajat.sharma@peak.ai>
2020-11-03 14:56:29 +0530

    remove import

--- src/main/java/io/confluent/connect/jdbc/dialect/GenericDatabaseDialect.java
+++ src/main/java/io/confluent/connect/jdbc/dialect/GenericDatabaseDialect.java
@@ -101,7 +101,6 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 
 /**
  * A {@link DatabaseDialect} implementation that provides functionality based upon JDBC and SQL.

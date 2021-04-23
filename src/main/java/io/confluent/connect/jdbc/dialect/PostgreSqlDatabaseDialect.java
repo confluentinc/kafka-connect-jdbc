c8cc0a417957d12be633e2afd3933ee7cfe2a967
Jeremy Custenborder <jcustenborder@gmail.com>
2021-04-22 22:33:25 -0500

    FF-2998: Postgres primitive array support. (#1044)
    * Took over PR #805 started by @Faqa. Great start! Thanks for your contribution. Removed new dependency to commons.lang. Integration tests were broken because task.initialize() wasn't being called. Added integration test. Fixes FF-2998. Closes #805.
    
    * Enable -Werror again.
    
    * Added DDL support and evolve test.
    
    * Refactored array code path and list code path to be the same.
    
    * Removed AUTO_CREATE because it's not needed.
    
    * Update src/main/java/io/confluent/connect/jdbc/dialect/PostgreSqlDatabaseDialect.java
    
    Co-authored-by: Nigel Liang <nigel@nigelliang.com>

--- src/main/java/io/confluent/connect/jdbc/dialect/PostgreSqlDatabaseDialect.java
+++ src/main/java/io/confluent/connect/jdbc/dialect/PostgreSqlDatabaseDialect.java
@@ -15,6 +15,7 @@
 
 package io.confluent.connect.jdbc.dialect;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import org.apache.kafka.common.config.AbstractConfig;
@@ -46,6 +47,7 @@ import io.confluent.connect.jdbc.util.ExpressionBuilder.Transform;
 import io.confluent.connect.jdbc.util.IdentifierRules;
 import io.confluent.connect.jdbc.util.TableDefinition;
 import io.confluent.connect.jdbc.util.TableId;
+import org.apache.kafka.connect.errors.DataException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -237,7 +239,6 @@ public class PostgreSqlDatabaseDialect extends GenericDatabaseDialect {
     }
     switch (field.schemaType()) {
       case INT8:
-        return "SMALLINT";
       case INT16:
         return "SMALLINT";
       case INT32:
@@ -254,6 +255,13 @@ public class PostgreSqlDatabaseDialect extends GenericDatabaseDialect {
         return "TEXT";
       case BYTES:
         return "BYTEA";
+      case ARRAY:
+        SinkRecordField childField = new SinkRecordField(
+              field.schema().valueSchema(),
+              field.name(),
+              field.isPrimaryKey()
+            );
+        return getSqlType(childField) + "[]";
       default:
         return super.getSqlType(field);
     }
@@ -364,6 +372,79 @@ public class PostgreSqlDatabaseDialect extends GenericDatabaseDialect {
     } else {
       super.formatColumnValue(builder, schemaName, schemaParameters, type, value);
     }
+  }
+
+  @Override
+  protected boolean maybeBindPrimitive(
+      PreparedStatement statement,
+      int index,
+      Schema schema,
+      Object value
+  ) throws SQLException {
+
+    switch (schema.type()) {
+      case ARRAY: {
+        Class<?> valueClass = value.getClass();
+        Object newValue = null;
+        Collection<?> valueCollection;
+        if (Collection.class.isAssignableFrom(valueClass)) {
+          valueCollection = (Collection<?>) value;
+        } else if (valueClass.isArray()) {
+          valueCollection = Arrays.asList((Object[]) value);
+        } else {
+          throw new DataException(
+              String.format("Type '%s' is not supported for Array.", valueClass.getName())
+          );
+        }
+
+        // All typecasts below are based on pgjdbc's documentation on how to use primitive arrays
+        // - https://jdbc.postgresql.org/documentation/head/arrays.html
+        switch (schema.valueSchema().type()) {
+          case INT8: {
+            // Gotta do this the long way, as Postgres has no single-byte integer,
+            // so we want to cast to short as the next best thing, and we can't do that with
+            // toArray.
+
+            newValue = valueCollection.stream()
+                .map(o -> ((Byte) o).shortValue())
+                .toArray(Short[]::new);
+            break;
+          }
+          case INT32:
+            newValue = valueCollection.toArray(new Integer[0]);
+            break;
+          case INT16:
+            newValue = valueCollection.toArray(new Short[0]);
+            break;
+          case BOOLEAN:
+            newValue = valueCollection.toArray(new Boolean[0]);
+            break;
+          case STRING:
+            newValue = valueCollection.toArray(new String[0]);
+            break;
+          case FLOAT64:
+            newValue = valueCollection.toArray(new Double[0]);
+            break;
+          case FLOAT32:
+            newValue = valueCollection.toArray(new Float[0]);
+            break;
+          case INT64:
+            newValue = valueCollection.toArray(new Long[0]);
+            break;
+          default:
+            break;
+        }
+
+        if (newValue != null) {
+          statement.setObject(index, newValue, Types.ARRAY);
+          return true;
+        }
+        break;
+      }
+      default:
+        break;
+    }
+    return super.maybeBindPrimitive(statement, index, schema, value);
   }
 
   /**

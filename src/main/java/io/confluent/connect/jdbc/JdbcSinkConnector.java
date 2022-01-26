d4422ad231e7cdba1338ee0914ad71b9efe8367e
Frank Z <fzhong@confluent.io>
2022-01-26 10:10:16 -0800

    failed style check

--- src/main/java/io/confluent/connect/jdbc/JdbcSinkConnector.java
+++ src/main/java/io/confluent/connect/jdbc/JdbcSinkConnector.java
@@ -84,7 +84,7 @@ public class JdbcSinkConnector extends SinkConnector {
         .ifPresent(deleteEnabled -> configValue(config, PK_MODE)
             .ifPresent(pkMode -> {
               if (!RECORD_KEY.name().toLowerCase(Locale.ROOT).equals(pkMode.value())
-              && !RECORD_KEY.name().toUpperCase(Locale.ROOT).equals(pkMode.value())) {
+                  && !RECORD_KEY.name().toUpperCase(Locale.ROOT).equals(pkMode.value())) {
                 String conflictMsg = "Deletes are only supported for pk.mode record_key";
                 pkMode.addErrorMessage(conflictMsg);
                 deleteEnabled.addErrorMessage(conflictMsg);

1ccd7ceed4d7910ada8cf7f071d81c0f3ae6d990
Fred R <frouleau@confluent.io>
2022-06-16 14:50:00 +0200

    Allow close() on CachedConnectionProvider while attempting to connect (#1205)

--- src/main/java/io/confluent/connect/jdbc/util/CachedConnectionProvider.java
+++ src/main/java/io/confluent/connect/jdbc/util/CachedConnectionProvider.java
@@ -37,6 +37,7 @@ public class CachedConnectionProvider implements ConnectionProvider {
 
   private int count = 0;
   private Connection connection;
+  private volatile boolean isRunning = true;
 
   public CachedConnectionProvider(
       ConnectionProvider provider
@@ -87,7 +88,7 @@ public class CachedConnectionProvider implements ConnectionProvider {
 
   private void newConnection() throws SQLException {
     int attempts = 0;
-    while (attempts < maxConnectionAttempts) {
+    while (isRunning) {
       try {
         ++count;
         log.info("Attempting to open connection #{} to {}", count, provider);
@@ -96,7 +97,7 @@ public class CachedConnectionProvider implements ConnectionProvider {
         return;
       } catch (SQLException sqle) {
         attempts++;
-        if (attempts < maxConnectionAttempts) {
+        if (isRunning && attempts < maxConnectionAttempts) {
           log.info("Unable to connect to database on attempt {}/{}. Will retry in {} ms.", attempts,
                    maxConnectionAttempts, connectionRetryBackoff, sqle
           );
@@ -113,7 +114,12 @@ public class CachedConnectionProvider implements ConnectionProvider {
   }
 
   @Override
-  public synchronized void close() {
+  public void close() {
+    isRunning = false;
+    closeConnection();
+  }
+
+  private synchronized void closeConnection() {
     if (connection != null) {
       try {
         log.info("Closing connection #{} to {}", count, provider);

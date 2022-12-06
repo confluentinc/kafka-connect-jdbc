64c759189ee4ad621a5418394d312822dec1b9ae
Snehashis <snpal@confluent.io>
2022-12-07 02:17:40 +0530

    Remove unwanted changes

--- src/main/java/io/confluent/connect/jdbc/sink/PreparedStatementBinder.java
+++ src/main/java/io/confluent/connect/jdbc/sink/PreparedStatementBinder.java
@@ -82,8 +82,7 @@ public class PreparedStatementBinder implements StatementBinder {
   }
 
   @Override
-  public void
-  bindRecord(SinkRecord record) throws SQLException {
+  public void bindRecord(SinkRecord record) throws SQLException {
     final Struct valueStruct = (Struct) record.value();
     final boolean isDelete = isNull(valueStruct);
     // Assumption: the relevant SQL has placeholders for keyFieldNames first followed by

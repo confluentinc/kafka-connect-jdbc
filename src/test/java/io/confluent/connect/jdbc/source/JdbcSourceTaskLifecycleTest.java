d9b8fc0b5a2388c5a6121e8cedda2a92dde51b0d
Chaitanya Mukka <chaitanya.mvs2007@gmail.com>
2023-02-08 10:42:11 +0530

    [Minor] Fix upstream tests failing due to #1284 (#1308)

--- src/test/java/io/confluent/connect/jdbc/source/JdbcSourceTaskLifecycleTest.java
+++ src/test/java/io/confluent/connect/jdbc/source/JdbcSourceTaskLifecycleTest.java
@@ -78,7 +78,7 @@ public class JdbcSourceTaskLifecycleTest extends JdbcSourceTaskTestBase {
 
     // Should request a connection, then should close it on stop()
     EasyMock.expect(mockCachedConnectionProvider.getConnection()).andReturn(db.getConnection()).anyTimes();
-    mockCachedConnectionProvider.close();
+    mockCachedConnectionProvider.close(true);
 
     PowerMock.expectLastCall();
 
@@ -132,7 +132,7 @@ public class JdbcSourceTaskLifecycleTest extends JdbcSourceTaskTestBase {
 
     // Should request a connection, then should close it on stop()
     EasyMock.expect(mockCachedConnectionProvider.getConnection()).andReturn(db.getConnection());
-    mockCachedConnectionProvider.close();
+    mockCachedConnectionProvider.close(true);
 
     PowerMock.expectLastCall();
 

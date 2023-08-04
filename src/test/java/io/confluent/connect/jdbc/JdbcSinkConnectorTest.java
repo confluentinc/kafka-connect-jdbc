6564a18054d47e332e1c0a07e832a3e84ae6930a
Charles Provencher <cpro1245@gmail.com>
2023-08-04 15:07:14 -0400

    Update JdbcSinkConnectorTest.java

--- src/test/java/io/confluent/connect/jdbc/JdbcSinkConnectorTest.java
+++ src/test/java/io/confluent/connect/jdbc/JdbcSinkConnectorTest.java
@@ -11,6 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations under the License.
+ * test CLA signing
  */
 
 package io.confluent.connect.jdbc;

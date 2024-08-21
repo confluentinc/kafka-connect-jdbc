79f4d981be599ee90d7cf2247592a45ceeea732d
aqeelbhattigr8 <aqeelbhattigr8@gmail.com>
2024-08-21 18:22:58 +0500

    improved filter condition

--- src/main/java/io/confluent/connect/jdbc/gp/GpDataIngestionService.java
+++ src/main/java/io/confluent/connect/jdbc/gp/GpDataIngestionService.java
@@ -147,12 +147,9 @@ public abstract class GpDataIngestionService implements IGPDataIngestionService 
 
         // add all columns except the updateExcludeColumns to updateColumnsList, excluded columns may have fully qualified names like tablename.columnname
         List<String> excludedColumns = config.updateExcludeColumns.stream()
-                .map(String::trim)
                 .filter(column -> column.contains(".") ? column.split("\\.").length > 1 && column.split("\\.")[0].equals(tableName) : true)
-                .map(column -> {
-                    String[] parts = column.split("\\.", 2);
-                    return parts.length > 1 ? parts[1] : parts[0];
-                })
+                .map(column -> column.contains(".") ? column.split("\\.")[1] : column )
+                .map(String::trim)
                 .collect(Collectors.toList());
 
         if (config.printDebugLogs) {
@@ -173,12 +170,9 @@ public abstract class GpDataIngestionService implements IGPDataIngestionService 
 
         // add all columns except the insertExcludeColumns to insertColumnsList, excluded columns may have fully qualified names like tablename.columnname
         excludedColumns =config.insertExcludeColumns.stream()
-                .map(String::trim)
                 .filter(column -> column.contains(".") ? column.split("\\.").length > 1 && column.split("\\.")[0].equals(tableName) : true)
-                .map(column -> {
-                    String[] parts = column.split("\\.", 2);
-                    return parts.length > 1 ? parts[1] : parts[0];
-                })
+                .map(column -> column.contains(".") ? column.split("\\.")[1] : column )
+                .map(String::trim)
                 .collect(Collectors.toList());
 
         if (config.printDebugLogs) {

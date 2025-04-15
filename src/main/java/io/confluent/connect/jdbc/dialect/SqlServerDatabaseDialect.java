0d9fd65483dda92001935d97d694f3a375bca6bb
KGaneshDatta <kdatta@confluent.io>
2025-04-15 16:22:16 +0530

    Fix Checkstyle Errors

--- src/main/java/io/confluent/connect/jdbc/dialect/SqlServerDatabaseDialect.java
+++ src/main/java/io/confluent/connect/jdbc/dialect/SqlServerDatabaseDialect.java
@@ -572,6 +572,7 @@ public class SqlServerDatabaseDialect extends GenericDatabaseDialect {
 
   @Override
   public String resolveSynonym(Connection connection, String synonymName) throws SQLException {
-    throw new SQLException("Sql Server Database does not support synonyms. Please use views instead.");
+    throw new SQLException(
+        "Sql Server Database does not support synonyms. Please use views instead.");
   }
 }
